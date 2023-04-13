6ab1aff7c1d4c0c7ae0e0fabcab70f332e7ebc65
smilewonjin <smilewonjin@gmail.com>
2023-04-13 10:55:38 +0900

    1

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>ELISHA</title>
+<title>smilewonjin</title>
 </head>
 <body>
 <!-- 인트로 시작 -->
 <div style="z-index:1;position:relative;min-width:970px">
     <div style="position:relative;margin:0 auto;padding:50px 0;width:970px;zoom:1;text-align:center">
-        <p><img src="index.gif" style="max-width:100%; height:auto"></p>
+        <p><img src="/images/logo.png" style="max-width:100%; height:auto"></p>
         <h1>안녕하세요</h1>
         <h2><a href="index.php">Go Home</a></h2>
     </div>
