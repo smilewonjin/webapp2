54eebcf78c55785b57ddcfabb5a32b2bd6e64cd3
smilewonjin <smilewonjin@gmail.com>
2023-04-13 11:11:47 +0900

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <div style="z-index:1;position:relative;min-width:970px">
     <div style="position:relative;margin:0 auto;padding:50px 0;width:970px;zoom:1;text-align:center">
         <p><img src="/images/logo.png" style="max-width:100%; height:auto"></p>
-        <h1>안녕하세요</h1>
+        <h1><font color="white">Hello World</font></h1>
         <h2><a href="index.php">Go Home</a></h2>
     </div>
 </div>
