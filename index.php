1679e3ee6665d909df8b4d3d2ae133d19e6efa65
smilewonjin <smilewonjin@gmail.com>
2023-04-13 10:40:23 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,19 @@
-<?php
-// 로그인 되어 있으면 목록으로 이동
-session_start();
-if (isset($_SESSION['member_id'])){
-    header("Location: /list.php");
-    exit();
-}
-
-// 서비스 소개
-?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>php-memo 목록</title>
-    </head>
-    <body>
-        <?php require_once("inc/header.php"); ?>
-        <h1>php-memo 첫 페이지</h1>
-    </body>
+<head>
+<meta charset="UTF-8">
+<title>ELISHA</title>
+</head>
+<body>
+<!-- 인트로 시작 -->
+<div style="z-index:1;position:relative;min-width:970px">
+    <div style="position:relative;margin:0 auto;padding:50px 0;width:970px;zoom:1;text-align:center">
+        <p><img src="index.gif" style="max-width:100%; height:auto"></p>
+        <h1>안녕하세요</h1>
+        <h2><a href="index.php">Go Home</a></h2>
+    </div>
+</div>
+<!-- <meta http-equiv='refresh' content='5; url=index.php'> -->
+<!-- } 인트로 끝 -->
+</body>
 </html>
